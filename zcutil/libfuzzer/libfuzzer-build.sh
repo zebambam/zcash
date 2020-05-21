15325ae330c3d2eddec8706cd9a8a1084e163b80
bambam <bambam@z.cash>
2020-05-21 22:47:18 +0000

    Added cli options parsing.

--- zcutil/libfuzzer/libfuzzer-build.sh
+++ zcutil/libfuzzer/libfuzzer-build.sh
@@ -1,11 +1,110 @@
 #!/usr/bin/env bash
-#   ./zcutil/libfuzzer/libfuzzer-build.sh <fuzz case>
 
-set -xeu -o pipefail
+set -eu -o pipefail
 
-FUZZ_CASE="$1"
-shift 
-export FUZZER_LOG_DIR="$(pwd)"
+usage() {
+  echo ""
+  echo "$0 <build stage> <options> <passthrough build arguments>"
+  echo ""
+  echo "Build a fuzzer in the local repo using the following options:"
+  echo ""
+  echo "<build stage>:" 
+  echo " either \"depends\" or \"fuzzer\""
+  echo ""
+  echo "<options>:"
+  echo " -f,--fuzzer <fuzzername> (ignored if building \"depends\")"
+  echo " [-s,--sanitizers <sanitizers>]"
+  echo " [-i,--instrument <instrument>]"
+  echo " [-h,--help]"
+  echo ""
+  echo "Where fuzzer is an entry in ./src/fuzzing/*, the default sanitizer"
+  echo "is \"address\" and default instrument is ( \"^.*/src/$\" )."
+  echo ""
+  exit -1
+}
+
+die() {
+  echo $1
+  exit -1
+}
+
+# parse command line options
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key = "$1"
+
+case $key in
+  -f|--fuzzer)
+  FUZZER_NAME="$2"
+  shift
+  shift
+  ;;
+  -s|--sanitizers)
+  LLVM_SANITIZERS="$2"
+  shift
+  shift
+  ;;
+  -i|--instrument)
+  INSTRUMENT_CODE="$2"
+  shift
+  shift
+  ;;
+  -h|--help)
+  usage
+  shift
+  ;;
+  *)
+  POSITIONAL+=("$1")
+  shift
+  ;;
+esac
+done
+
+# positional arguments
+
+if [ "${#POSITIONAL[@]}" != 1 ]
+then
+  usage
+else
+  BUILD_STAGE=${POSITIONAL[0]}
+fi
+
+case "${BUILD_STAGE:-undefined}" in
+  depends)
+  # fine
+  ;;
+  fuzzer)
+  FUZZER_NAME=notused
+  # fine
+  ;;
+  *)
+  # not fine
+  usage
+  ;;
+esac
+
+# required arguments
+
+if [ "${FUZZER_NAME:-undefined}" = "undefined" ]
+then
+  usage
+fi
+
+# default values
+
+if [ "${LLVM_SANITIZERS:-undefined}" = "undefined" ]
+then
+  export LLVM_SANITIZERS="address"
+fi
+if [ "${INSTRUMENT_CODE:-undefinex}" = "undefined" ]
+then
+  export INSTRUMENT_CODE=("^.*\/src")
+fi
+
+set -x
+
 export ZCUTIL=$(realpath "./zcutil")
 
 # overwrite the Linux make profile to use clang instead of GCC:
@@ -13,16 +112,26 @@ export ZCUTIL=$(realpath "./zcutil")
 cat $ZCUTIL/../depends/hosts/linux.mk | sed -e 's/=gcc/=clang-7/g' | sed -e 's/=g++/=clang++-7/g' > x
 mv x $ZCUTIL/../depends/hosts/linux.mk
 
-# don't modify the filesystem if we're only building dependencies
-# this is to optimize docker layers
+# the build_stage distinction helps to layer an intermediate docker 
+# container for the built dependencies, so we can resume building 
+# from there assuming no other build arguments have changed
 
-if [ "$FUZZ_CASE" = "dependsonly" ]
+if [ "$BUILD_STAGE" = "depends" ]
 then
   # make an empty fuzz file just so we can build dependencies 
   > src/fuzz.cpp
-  export BUILD_STAGE=dependsonly
 else
-  cp "./src/fuzzing/$FUZZ_CASE/fuzz.cpp" src/fuzz.cpp
+  cp "./src/fuzzing/$FUZZ_CASE/fuzz.cpp" src/fuzz.cpp || die "Can't copy fuzz.cpp for that fuzzer"
 fi
 
-CONFIGURE_FLAGS="--enable-tests=no --disable-bench" "$ZCUTIL/build.sh" -j$(nproc) "CC=$ZCUTIL/libfuzzer/zcash-wrapper-clang" "CXX=$ZCUTIL/libfuzzer/zcash-wrapper-clang++" "$@"
+# sneak the variable into zcashd's build.sh
+
+export BUILD_STAGE
+
+# run build.sh with our compiler wrapper, and BUILD_STAGE environment set:
+
+CONFIGURE_FLAGS="--enable-tests=no --disable-bench" \
+  "$ZCUTIL/build.sh" \
+  -j$(nproc) \
+  "CC=$ZCUTIL/libfuzzer/zcash-wrapper-clang" \
+  "CXX=$ZCUTIL/libfuzzer/zcash-wrapper-clang++" "${POSITIONAL[@]:1}" || die "Build failed."
