53028342fce126105e6054d6c1fe10a8eb8af601
zebambam <bambam@z.cash>
2020-08-05 21:54:34 +0000

    Added database opening and stringstream dumping from verify.

--- src/fuzzing/bdb__verify/fuzz.cpp
+++ src/fuzzing/bdb__verify/fuzz.cpp
@@ -17,19 +17,20 @@ void createRamDiskMount() {
 }
 
 // We write one file per pid inside /mnt/ram
-const std::string filename = std::to_string(getpid()) + std::string(".fuzzdata");
+const std::string filename = std::to_string("/mnt/ram/") + std::to_string(getpid()) + std::string(".fuzzdata");
 
 void rewriteTestFile(const uint8_t *Data, size_t Size) {
-  std::ofstream ofs;
-  ofs.open(filename, ios::binary | ios::out | ios::trunc);
+  std::ofstream ofs (filename, ios::binary, ios::out | ios::trunc );
   ofs.write((const char *) Data, (streamsize) Size);
   ofs.flush();
   ofs.close();
 }
 
+DBEnv *dbenv;
+
 int fuzz_bdb_verify(const uint8_t *Data, size_t Size) {
   // ramdisk is only 25 meg big
-  if (Size > 20 * (1<<20)) {
+  if (Size > (20 * (1<<20))) {
     return -1;
   }
   rewriteTestFile(Data, Size);
@@ -40,10 +41,34 @@ int fuzz_bdb_verify(const uint8_t *Data, size_t Size) {
 
   u_int32_t flags = DB_SALVAGE;
 
-  DbEnv *dbenv;
+  dbenv->set_lg_dir((filename + ".log").c_str())
+  dbenv->set_cachesize(0, 0x100000, 1); // 1 MiB should be enough for just the wallet
+  dbenv->set_lg_bsize(0x10000);
+  dbenv->set_lg_max(1048576);
+  dbenv->set_lk_max_locks(40000);
+  dbenv->set_lk_max_objects(40000);
+  dbenv->set_errfile(fopen(filename+".error").c_str(), "a")); /// debug
+  dbenv->set_flags(DB_AUTO_COMMIT, 1);
+  dbenv->set_flags(DB_TXN_WRITE_NOSYNC, 1);
+  dbenv->log_set_config(DB_LOG_AUTO_REMOVE, 1);
+  int ret = dbenv->open(filename.c_str(),
+     DB_CREATE |
+     DB_INIT_LOCK |
+     DB_INIT_LOG |
+     DB_INIT_MPOOL |
+     DB_INIT_TXN |
+     DB_THREAD |
+     DB_RECOVER |
+     nEnvFlags,
+     S_IRUSR | S_IWUSR);
+  if (ret != 0)
+    return error("CDBEnv::Open: Error %d opening database environment: %s\n", ret, DbEnv::strerror(ret));
+
   Db db(dbenv, 0);
   result = db.verify(filename.c_str(), NULL, &strDump, flags);
+  cout << strDump.str() << endl;
   result = db.verify(filename.c_str(), NULL, &strDump, flags | DB_AGGRESSIVE);
+  cout << strDump.str() << endl;
   return result;
 }
 
